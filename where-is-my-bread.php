ed278fc6e3df09813d8e2a87188ee53fed33797c
amarinediary <bp.brizzi@gmail.com>
2021-05-06 22:18:09 +0200

    Update where-is-my-bread.php

--- where-is-my-bread.php
+++ where-is-my-bread.php
@@ -1,1 +1,24 @@
+<?php
 
+if ( ! defined( 'ABSPATH' ) ) {
+
+    exit;
+
+};
+
+/**
+ * Plugin Name: Where-Is-My-Bread
+ * Text Domain: where-is-my-bread
+ * Plugin URI: https://github.com/amarinediary/Where-Is-My-Bread
+ * Description: A non-invasive, lightweight WordPress plugin adding url based breadcrumb support. Where-Is-My-Bread is a plug-and-play plugin with no required configuration.
+ * Version: 1.0.0
+ * Requires at least: 4.8.0
+ * Requires PHP: 4.0
+ * Tested up to: 5.7.1
+ * Author: amarinediary
+ * Author URI: https://github.com/amarinediary
+ * License: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication
+ * License URI: https://github.com/amarinediary/Where-Is-My-Bread/blob/main/LICENSE
+ * GitHub Plugin URI: https://github.com/amarinediary/Where-Is-My-Bread
+ * GitHub Branch: main
+ */
