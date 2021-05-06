456264acc9b0749a5535faddbc81f36207d265a7
amarinediary <bp.brizzi@gmail.com>
2021-05-06 23:39:23 +0200

    na

--- where-is-my-bread.php
+++ where-is-my-bread.php
@@ -9,16 +9,89 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Plugin Name: Where Is My Bread 🍞
  * Text Domain: where-is-my-bread
- * Plugin URI: https://github.com/amarinediary/Where-Is-My-Bread/
+ * Plugin URI: https://github.com/amarinediary/Where-Is-My-Bread
  * Description: A non-invasive, lightweight WordPress plugin adding url based breadcrumb support. Where Is My Bread 🍞 is a plug-and-play plugin with no required configuration.
  * Version: 1.0.0
- * Requires at least: 4.8.0
- * Requires PHP: 4.0
+ * Requires at least: 5.6.0
+ * Requires PHP: 8.0
  * Tested up to: 5.7.1
  * Author: amarinediary
- * Author URI: https://github.com/amarinediary/
+ * Author URI: https://github.com/amarinediary
  * License: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication
- * License URI: https://github.com/amarinediary/Where-Is-My-Bread/blob/main/LICENSE/
- * GitHub Plugin URI: https://github.com/amarinediary/Where-Is-My-Bread/
+ * License URI: https://github.com/amarinediary/Where-Is-My-Bread/blob/main/LICENSE
+ * GitHub Plugin URI: https://github.com/amarinediary/Where-Is-My-Bread
  * GitHub Branch: main
  */
+if ( ! class_exists( 'Where_Is_My_Bread' ) ) {
+
+    class Where_Is_My_Bread {
+
+        /**
+         * Hooks methods to actions.
+         *
+         * @since 1.0.0
+         */
+        public function __construct() {
+
+            add_action( 'wp', array( $this, 'get_crumbs' ) );
+
+            add_action( 'wp', array( $this, 'get_past_crumb' ) );
+
+        }
+
+        /**
+         * Retrieve all crumbs.
+         *
+         * @since 1.0.0
+         * 
+         * @return Array Crumbs array.
+         */
+        public function get_crumbs() {
+
+            
+            
+        }
+
+        /**
+         * Retrieve the bread as a formated crumbs list.
+         *
+         * @since 1.0.0
+         * 
+         * @return Array Crumbs list.
+         */
+        public function get_bread() {
+        
+            return '<ol class="🍞 bread">';
+
+            $crumbs = $this->get_crumbs();
+
+            foreach ( $crumbs as $crumb ) {
+
+                return '<li class="crumb"><a href="' . $crumb->url . '">' . $crumb->slug . '</a></li>';
+
+            };
+
+            return '</ol>' . $args->after_ol . '';
+        
+        }
+        
+        /**
+         * Retrieve past crumb slug|URL.
+         *
+         * @since 1.0.0
+         * 
+         * @param String Accept either slug|URL.
+         * 
+         * @return String Previous crumb slug|URL.
+         */
+        public function get_past_crumb() {
+        
+
+        
+        }
+        
+    };
+
+    $where_is_my_bread = new Where_Is_My_Bread();
+
+};
