1b05e04032cd2384214bc016be0f0c58b86b6a5c
amarinediary <bp.brizzi@gmail.com>
2021-11-30 01:54:55 +0100

    Update where-is-my-bread.php

--- where-is-my-bread.php
+++ where-is-my-bread.php
@@ -10,7 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Plugin Name: Where's My Bread ? 🍞
  * Text Domain: where-is-my-bread
  * Plugin URI: https://github.com/amarinediary/Where-Is-My-Bread
- * Description: A URL based WordPress breadcrumb, unstyled, minimalist and SEO friendly. A non-invasive WordPress unofficial plugin, both lightweight and lightning fast, adding URL based breadcrumb support. Plug-and-play, with no required configuration.
+ * Description: A URL based WordPress breadcrumb, unstyled, minimalist and SEO friendly. A non-invasive WordPress plugin, both lightweight and lightning fast, adding URL based breadcrumb support. A plug-and-play plugin, with no required configuration.
  * Version: 1.0.2
  * Requires at least: 5.6.0
  * Requires PHP: 8.0
@@ -42,8 +42,11 @@ if ( ! function_exists( 'get_the_crumbs' ) ) {
 
         $flour = $_SERVER['REQUEST_URI'];
 
-        if ( str_contains( $flour, '?' ) )
+        if ( str_contains( $flour, '?' ) ) {
+
             $flour = substr( $flour, 0, strpos( $flour, '?' ) );
+
+        };
 
         $flour = ( str_ends_with( $flour, '/' ) ? explode( '/', substr( $flour, 1, -1 ) ) : explode( '/', substr( $flour, 1 ) ) );
 
@@ -55,12 +58,10 @@ if ( ! function_exists( 'get_the_crumbs' ) ) {
 
             $url = esc_url( $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/' . substr( implode( '/', $flour ), 0, strpos( implode( '/', $flour ), $crumb ) ) . $crumb. '/' );
 
-            array_push( $crumbs, ( object )
-                [
-                    'slug' => $slug,
-                    'url' => $url,
-                ]
-            );
+            array_push( $crumbs, (object) array(
+                'slug' => $slug,
+                'url' => $url,
+            ) );    
 
         };
 
@@ -77,22 +78,24 @@ if ( ! function_exists( 'the_bread' ) ) {
      * 
      * @since 1.0.0
      * 
-     * @param Array $ingredients[separator] The crumb's separator. Default to >.
-     * @param Array $ingredients[offset] Crumbs offset. Accept positive/negative Integer. Default to 0. Refer to array_slice. https://www.php.net/manual/en/function.array-slice.php.
-     * @param Array $ingredients[length] Crumbs length. Accept positive/negative Integer. Default to null. Refer to array_slice. https://www.php.net/manual/en/function.array-slice.php.
+     * @param Array $ingredients[separator] The crumb's separator. Default to ">".
+     * @param Array $ingredients[offset] Crumbs offset. Accept positive/negative Integer. Default to "0". Refer to array_slice, https://www.php.net/manual/en/function.array-slice.php.
+     * @param Array $ingredients[length] Crumbs length. Accept positive/negative Integer. Default to "null". Refer to array_slice, https://www.php.net/manual/en/function.array-slice.php.
      * 
      * @return Array The formatted crumbs list.
      */
     function the_bread(
-        $ingredients = [
+        $ingredients = array(
             'separator' => '>',
             'offset' => 0,
             'length' => null,
-        ]
-    ) { 
+        )
+    ) {
 
-        $offset =  ( empty( $ingredients['offset'] ) ? 0 : $ingredients['offset'] );
-        $length =  ( empty( $ingredients['length'] ) ? null : $ingredients['length'] );
+        $offset = ( empty( $ingredients['offset'] ) ? 0 : $ingredients['offset'] );
+
+        $length = ( empty( $ingredients['length'] ) ? null : $ingredients['length'] );
+
         $crumbs = array_slice( get_the_crumbs(), $offset, $length );
 
         if ( ! empty( $crumbs ) ) {
@@ -100,10 +103,12 @@ if ( ! function_exists( 'the_bread' ) ) {
             echo '<ol class="🍞 bread" itemscope itemtype="https://schema.org/BreadcrumbList">';
 
             $i = 0;
+
             foreach ( $crumbs as $crumb ) {
+
                 $i++;
                 
-                //get_page_by_path() usage credit to https://github.com/mohamadnr
+                //get_page_by_path() usage, credit to https://github.com/mohamadnr
                 echo '<li class="crumb" itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
                     <a itemprop="item" href="' . $crumb->url . '">
                     <span itemprop="name">' . ( url_to_postid( $crumb->url ) ? get_the_title( url_to_postid( $crumb->url ) ) : ( get_page_by_path( $crumb->slug )->ID ? get_the_title( get_page_by_path( $crumb->slug )->ID ) : ucfirst( str_replace( '-', ' ', $crumb->slug ) ) ) ) . '</span>
@@ -111,8 +116,11 @@ if ( ! function_exists( 'the_bread' ) ) {
                     <meta itemprop="position" content="' . $i . '">
                 </li>';
     
-                if ( $i !== sizeof( $crumbs ) && ! empty( $ingredients['separator'] ) )
+                if ( $i !== sizeof( $crumbs ) && ! empty( $ingredients['separator'] ) ) {
+
                     echo $ingredients['separator'];
+
+                };
     
             };
     
